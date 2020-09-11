fef50fedbba0d2ebd104bc59c8609b88cc8cd93a
Artur Nikitin <branewor@gmail.com>
2020-09-11 13:56:36 +0400

    прикрутил слайдер

--- js/main.js
+++ js/main.js
@@ -3,6 +3,74 @@ $(document).ready(function(){
         infinite: true,
         slidesToShow: 4,
         slidesToScroll: 1,
+        arrows: true,
+        dots: true,
+        responsive: [
+            {
+                breakpoint: 1199,
+                settings: {
+                  arrows: false,
+                  slidesToShow: 3
+                }
+              },
+            {
+              breakpoint: 768,
+              settings: {
+                arrows: false,
+                slidesToShow: 2
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                arrows: false,
+                slidesToShow: 1
+              }
+            }
+          ]
+
     })
+
+    $('.blog__slider').slick({
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        arrows: false,
+        responsive: [
+            {
+                breakpoint: 1199,
+                settings: {
+                  arrows: false,
+                  dots: true,
+
+                  slidesToShow: 3
+                }
+              },
+            {
+              breakpoint: 768,
+              settings: {
+                arrows: false,
+                dots: true,
+
+                slidesToShow: 2
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                arrows: false,
+                dots: true,
+
+                slidesToShow: 1
+              }
+            }
+          ]
+
+      });
+
+    
+
+
+
   })
       
