a08b1bf334de71488b691f5763109e5045cb1cb0
Artur Nikitin <branewor@gmail.com>
2020-09-14 15:30:55 +0400

    плавный скрол и прилипающее меню

--- js/main.js
+++ js/main.js
@@ -21,7 +21,7 @@ $(document).ready(function(){
               }
             },
             {
-              breakpoint: 480,
+              breakpoint: 650,
               settings: {
                 arrows: false,
                 slidesToShow: 1
@@ -56,7 +56,7 @@ $(document).ready(function(){
               }
             },
             {
-              breakpoint: 480,
+              breakpoint: 650,
               settings: {
                 arrows: false,
                 dots: true,
@@ -67,6 +67,45 @@ $(document).ready(function(){
           ]
 
       });
+      // fixed heafer 
+      let header = $('#header')
+      let intro = $('#intro')
+      let introH = intro.innerHeight()
+      let scrollPos = $(window).scrollTop()
+      $(window).on('scroll', function () {
+        scrollPos = $(this).scrollTop()
+
+        if(scrollPos > introH) {
+          header.addClass('fixed')
+        } else {
+          header.removeClass('fixed')
+        }
+      })
+
+      // smooth scroll 
+      $('[data-scroll]').on('click', function (event){
+        event.preventDefault();
+
+        let elementID = $(this).data('scroll');
+        let elementOffset = $(elementID).offset().top;
+
+        console.log(elementOffset);
+        $('html, body').animate({
+          scrollTop: elementOffset
+        })
+
+
+
+
+      })
+
+
+      
+
+
+
+      
+
 
     
 
